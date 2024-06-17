b4e20fd67da2f753cc253fea85f05f5ff06786d7
Akagi <118636456+AkagiMT@users.noreply.github.com>
2024-06-17 13:07:26 +0300

    Update app.js
    deleted testcase

--- app.js
+++ app.js
@@ -23,16 +23,7 @@ class Team {
 
 
 //testcase
-let team1 = new Team('unga');
-team1.score += 10
-let team2 = new Team('bunga');
-let team3 = new Team('bunga');
-team3.score += 30
-team2.score += 20
-let teams = [];
-teams.push(team1);
-teams.push(team2);
-teams.push(team3);
+
 
 //team unga / score 10 / score history []
 
